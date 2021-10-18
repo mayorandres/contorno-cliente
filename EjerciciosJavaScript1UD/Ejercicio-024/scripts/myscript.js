a239cf45f581de0faa0e734eaa42990bf7545df1
steve_trabajos <martin.listeboquete@gmail.com>
2021-10-18 15:58:50 +0200

    DONE 24 & 25

--- EjerciciosJavaScript1UD/Ejercicio-024/scripts/myscript.js
+++ EjerciciosJavaScript1UD/Ejercicio-024/scripts/myscript.js
@@ -10,3 +10,30 @@
 *
 *
 ***************************************************************************************************************/
+function leerNumero(){
+    let numero
+    do {
+        numero = prompt("Dame un número de segundos")
+    }while(isNaN(numero) ||!Number.isInteger(parseFloat(numero)) || parseInt(numero)<0)
+    return numero
+}
+
+function calcularTiempo(n){
+    let semana=parseInt(n/(3600*7*24))
+    let resto=n%(3600*7*24)
+    let dia=parseInt(resto/(3600*24))
+    resto%=(3600*24)
+    let hora=parseInt(resto/(3600))
+    resto%=(3600)
+    let minuto=parseInt(resto/60)
+    resto%=(60)
+   
+
+    return `Hemos estado confiandos ${semana} semanas, ${dia} días, ${hora} horas, ${minuto} minutos y ${resto} segundos`
+}
+// dia = 86400
+//semana = 604800 // 691200
+
+let n = leerNumero()
+
+console.log(calcularTiempo(n))
--- EjerciciosJavaScript1UD/Ejercicio-025/scripts/myscript.js
+++ EjerciciosJavaScript1UD/Ejercicio-025/scripts/myscript.js
@@ -10,3 +10,46 @@
 *
 *
 ***************************************************************************************************************/
+function leerNumero(){
+    let numero
+    do {
+        numero = prompt("Dame un número")
+    }while (isNaN(numero) || !Number.isInteger(parseFloat(numero)) || parseInt(numero) <0)
+    return numero
+}
+
+
+function numeroMayor(listaNumeros){
+    let max = 0
+    for (let i=0; i<listaNumeros.length; i++){
+        if (listaNumeros[i] > max){
+            max = listaNumeros[i]
+        }
+    }
+    return max
+}
+
+function recorrerArray(listado){ 
+    let salida = "El mayor de: "
+    for (let i=0; i<listado.length-1; i++){
+        salida += listado[i]+", "
+    }
+    salida += listado[listado.length-1] + " es: "
+    salida += numeroMayor(listado)
+    return salida;
+}
+
+let numeros= new Array()
+let num
+do{
+    
+    num = leerNumero()
+    if( num == 0){
+        break;
+    }
+    numeros.push(num)
+}while (num != 0)
+
+console.log(recorrerArray(numeros))
+
+
